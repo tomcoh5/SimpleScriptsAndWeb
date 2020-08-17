eac80888168b7f72a0381be930dc5d5261d85f9d
root <root@localhost.localdomain>
2020-08-18 01:37:58 +0300

    alert.sh

--- mysqlbackup/alert.sh
+++ mysqlbackup/alert.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 current_date=$(date "+%Y-%m-%d-%H:%M") >> /dev/null
+echo current date $current_date
 for f in *.SQL.gz; do
-	extract=${f:0:16}
+    extract=${f:0:16}
+    echo extract $extract
     year=${extract:0:4}
     month=${extract:5:2}
     day=${extract:8:2}
@@ -13,31 +15,25 @@ for f in *.SQL.gz; do
     end_ts=$(date -d "$currentdate" '+%s')
     day=$(( ( end_ts - start_ts )/(6060*24) ))
 
-    #time and hour extraction
+    #minutes and hour  extraction
+    hourAndminute=${extract:11}
+    date_only=${extract:0:10}
+    timestamp_file=`date -d "$date_only $hourAndminute" +"%s"`
+    timestamp_current=$(date +%s)
+    diff=$(( $timestamp_current - $timestamp_file ))
 
-    current_hour=${currentdate:11:16}
-    conv_current_hour=$(echo $current_hour | awk -F: '{ print ($1 * 3600) + ($2 * 60) }')
-    hour_minutes=$(echo $exe | awk -F: '{ print ($1 * 3600) + ($2 * 60) }')
-    diff=$(( $conv_current_hour - $hour_minutes ))
     #function to display hour and minutes diff 
-    diff=${diff:1}
-    function displaytime {
-  	local T=$1
-  	local D=$((T/60/60/24))
-  	local H=$((T/60/60%24))
-  	local M=$((T/60%60))
-  	local S=$((T%60))
-  	(( $H > 0 )) && printf '%d hours ' $H
-  	(( $M > 0 )) && printf '%d minutes ' $M
-  	(( $D > 0 || $H > 0 || $M > 0 )) && printf 'and '
-  	printf '%d seconds\n' $S
-     }
-
+    convertsecs() {
+ 	((h=${1}/3600))
+ 	((m=(${1}%3600)/60))
+ 	((s=${1}%60))
+ 	printf "%02d:%02d:%02d\n" $h $m $s
+	}    
 
 
 
     if [ $day -lt 1 ];then
-            echo "backup has been made before $(displaytime $diff)"
+            echo "backup has been made before $(convertsecs $diff) "
             return 1
             exit 0
     else
