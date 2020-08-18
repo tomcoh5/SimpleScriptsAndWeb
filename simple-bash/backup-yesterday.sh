da837178fafe7e9b4a06b5360d9a76e9f95aa021
tomcohen <66440442+tomcoh5@users.noreply.github.com>
2020-08-18 13:28:23 +0300

    Update backup-yesterday.sh

--- simple-bash/backup-yesterday.sh
+++ simple-bash/backup-yesterday.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
+current_date=$(date +"%m-%d-%y")
 file_path="/home/yuofvi/gopower"
 backup_dir="/root/backup"
 echo "Got it this is your file path $file_path"
 echo "Searching for files ..."
+touch $file_path/$current_date-backup.txt
 cd $file_path
-pwd
-if [[ -f "save.txt" ]]; then
-	rm -f save.txt
-fi
-touch save.txt
 echo "Saving all the files"
 echo "Making backup file"
 for file in `find $file_path -type f -mtime -7 -exec ls -l {} \; | awk '{print $9}'`;do
-	echo making back up for $file
-	tar -cvf $backupdir/$file.tar
-	echo $file >> save.txt
+        echo "adding $file for zip"
+        final_backup+="$file"
+        final_backup+=" "
+        echo "Overwriting backup.txt file if used today"
+        echo "" > $file_path/$current_date-backup.txt
+        echo $file >> $file_path/$current_date-backup.txt
 done
-
-	
+zip $backup_dir/$current_date-backup.zip $final_backup
