fc28ee4792ef87555bbf0168153fbb117384a2e3
tomcohen <66440442+tomcoh5@users.noreply.github.com>
2020-08-18 10:11:34 +0300

    Update backup-yesterday.sh

--- simple-bash/backup-yesterday.sh
+++ simple-bash/backup-yesterday.sh
@@ -5,11 +5,16 @@ echo "Got it this is your file path $file_path"
 echo "Searching for files ..."
 cd $file_path
 pwd
+if [[ -f "save.txt" ]]; then
+	rm -f save.txt
+fi
+touch save.txt
 echo "Saving all the files"
 echo "Making backup file"
 for file in `find $file_path -type f -mtime -7 -exec ls -l {} \; | awk '{print $9}'`;do
 	echo making back up for $file
 	tar -cvf $backupdir/$file.tar
+	echo $file >> save.txt
 done
 
 	
