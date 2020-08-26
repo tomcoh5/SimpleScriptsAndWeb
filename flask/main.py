fd60825a633f4de79d7d4c44590ab47e8fe572eb
tomcohen <66440442+tomcoh5@users.noreply.github.com>
2020-08-27 00:56:46 +0300

    Update main.py

--- flask/main.py
+++ flask/main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, render_template, redirect, url_for
 import subprocess
+import urllib.request
+ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+
 app = Flask(__name__)
 
 def deploycommand():
@@ -10,8 +13,16 @@ def deploycommand():
         return "deployed.. "
     except:
         return "Error cant deploy"
+def did():
+    global bashcommand
+    return bashcommand + "has been deployed you can check it out by doing docker ps -a"
 
-
+@app.route('/test')
+def test():
+    global bashcommand
+    bashcommand = "docker run "
+    did()
+    return redirect("http://" + ip + ":5000")
 
 @app.route('/')
 def my_form():
@@ -21,7 +32,7 @@ def my_form():
 def my_form_post():
     text = request.form['text']
     if text =="1":
-        return redirect("http://localhost:5000/container")
+        return redirect("http://"+ ip +":5000/container")
     else:
         return "Error 404"
 
@@ -30,9 +41,8 @@ def my_form_post():
 def plz():
     return render_template('container.html')
 
-@app.route('/container', methods=['POST'])
+@app.route('/container', methods=['POST', 'GET'])
 def plz_post():
-    global define
     global image
     global name
     global bashcommand
@@ -43,77 +53,72 @@ def plz_post():
     if image == "apache" or image == "nginx" or image == "httpd":
         bashcommand = "docker run -d --name " + name + " -p 8080:8080 " + image
         deploycommand()
+        return redirect("http://" + ip + ":5000")
     elif image == "grafana":
         bashcommand = "docker run -d -p 3000:3000 " + image
         deploycommand()
+        return redirect("http://" + ip + ":5000")
     elif image == "postgresql" or image == "postgres":
-        password = input("write down password for your database")
-        bashcommand = "docker run --name " + name + " -e POSTGRES_PASSWORD=" + password + " -d postgres"
-        deploycommand()
+        return redirect("http://"+ ip + ":5000/passwordpost")
     elif image == "mysql":
-        password = input("write down password for your database")
         bashcommand = "docker run --name " + name + " MYSQL_ROOT_PASSWORD=" + password + " -d mysql"
-        deploycommand()
+        return redirect("http://" + ip + ":5000/passwordmysql")
     else:
-        tellme = input("Do you need port for your image?")
-        if tellme == "quit":
-            running = False
-        elif tellme == "yes":
-            port = input("Write Down the port")
-            bashcommand = "docker run -d --name " + name + " -p " + port + "  " + image
-            deploycommand()
-        elif tellme == "no":
-            print("Lets run your container")
-            bashcommand = "docker run -d --name " + name + " " + image
-            deploycommand()
-
-    return redirect("http://localhost:5000/deployment")
+        return redirect("http://" + ip + ":5000/deployment")
 
 
-@app.route('/deployment', methods=['POST'])
-def ports():
-    return render_template()
+
+@app.route('/passwordpost')
+def password():
+    return render_template('password.html')
+
+@app.route('/passwordpost', methods=['POST', 'GET'])
+def password_post():
+    global bashcommand
+    password = request.form['text']
+    bashcommand = "docker run --name " + name + " -e POSTGRES_PASSWORD=" + password + " -d postgres"
+    deploycommand()
+    return redirect("http://" + ip + ":5000")
+
+@app.route('/passwordmysql')
+def passwordsql():
+    return render_template('passwordsql.html')
+
+@app.route('/passwordmysql', methods=['POST', 'GET'])
+def passwordsql_post():
+    global bashcommand
+    password = request.form['text']
+    bashcommand = "docker run --name " + name + " MYSQL_ROOT_PASSWORD=" + password + " -d mysql"
+    deploycommand()
+    return redirect("http://" + ip + ":5000")
+
+
+
+@app.route('/deployment')
+def deployment():
+    return render_template('deployment.html')
+
+@app.route('/deployment',  methods=['POST', 'GET'])
+def deployment_post():
+    global bashcommand
+    port = request.form['text']
+    if port == "no":
+        bashcommand = "docker run -d --name " + name + " " + image
+        deploycommand()
+        return redirect("http://" + ip + ":5000")
+    else:
+        bashcommand = "docker run -d --name " + name + " -p " + port + "  " + image
+        deploycommand()
+        return redirect("http://" + ip + ":5000")
+
+
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=5000,host="0.0.0.0")
 
 
 
-#some functions to make it easy
 
 
 
-def dockercontainer():
-    global running
-    global bashcommand
-    image = input("Tell me what image you want for your docker container")
-    name = input("and also write a name for your container ")
-    if image == "quit":
-        running = False
-    else:
-        if image == "apache" or image == "nginx" or image == "httpd":
-            bashcommand = "docker run -d --name " + name + " -p 8080:8080 " + image
-            deploycommand()
-        elif image == "grafana":
-            bashcommand = "docker run -d -p 3000:3000 " + image
-            deploycommand()
-        elif image == "postgresql" or image == "postgres":
-            password = input("write down password for your database")
-            bashcommand = "docker run --name " + name + " -e POSTGRES_PASSWORD=" + password + " -d postgres"
-            deploycommand()
-        elif image == "mysql":
-            password = input("write down password for your database")
-            bashcommand = "docker run --name " + name + " MYSQL_ROOT_PASSWORD=" + password + " -d mysql"
-            deploycommand()
-        else:
-            tellme = input("Do you need port for your image?")
-            if tellme == "quit":
-                running = False
-            elif tellme == "yes":
-                port = input("Write Down the port")
-                bashcommand = "docker run -d --name " + name + " -p " + port + "  " + image
-                deploycommand()
-            elif tellme == "no":
-                print("Lets run your container")
-                bashcommand = "docker run -d --name " + name + " " +  image
-                deploycommand()
